c274796403c074dc3507ed4ccb2f530507411f4a
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-02-28 18:15:31 +1100

    basic javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+// Single  comment
+//console.log("shhshshsh");
+
+/*Multiple line comment
+alert("hehehe");*/
+
+// Variable (only char, num, $ _)
+// Var  name = “prem”
+// Let name = “prem”
+// Const  lastName = “acharya”
+
+var name = "Bikal Adhikari";
+let add = "sydney";
+const dob = "2020-20-2";
+
+console.log(name, add, dob);
+
+name = "sam";
+add = "new york";
+// dob = "1998-05-30";
+console.log(name, add);
