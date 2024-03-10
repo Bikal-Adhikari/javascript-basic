4d2e7f0823faa03f46cd5526d94fb96ef6f562be
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-11 10:44:55 +1100

    OOP Abstraction

--- script-advance.js
+++ script-advance.js
@@ -32,24 +32,40 @@
 // This keywords
 // Constructor
 class Person {
+  #secKey;
   constructor(n, a) {
     this.name = n.toUpperCase();
     this.add = a.toUpperCase();
+    this.#secKey = "36737";
   }
   bio() {
-    return `hey, this is ${this.name} and I live in ${this.add}`;
+    return `hey, this is ${this.name} and I live in ${
+      this.add
+    }. secret key used was ${this.#secretKeyGenerator()}`;
+    // return `hey, this is ${this.name} and I live in ${
+    //   this.add
+    // }. secret key used was ${this.#secKey}`;
+  }
+
+  nameinLower() {
+    return this.name.toLowerCase();
+  }
+
+  #secretKeyGenerator() {
+    return "3456";
   }
 }
 
 const bikObj = new Person("Bikal", "Sydney");
 
 bikObj.ph = 98756;
-console.log(bikObj);
+// console.log(bikObj.#secKey);
+// console.log(bikObj.nameinLower());
 console.log(bikObj.bio());
 
-const jphnObj = new Person("Johhn", "London");
-// console.log(jphnObj);
-console.log(jphnObj.bio());
+// const jphnObj = new Person("Johhn", "London");
+// // console.log(jphnObj);
+// console.log(jphnObj.bio());
 
 // Encapsulation
 // Abstraction
