b538bc3e035be3a9a09e4155d98f87fa25f14924
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-11 10:04:16 +1100

    OOP class

--- script-advance.js
+++ script-advance.js
@@ -12,23 +12,38 @@
 
 // console.log(person.bio());
 
-const person = (name, add) => {
-  return {
-    personName: name,
-    address: add,
-    // bio: () => {
-    bio() {
-      return `The name is ${this.personName}`;
-    },
-  };
-};
+// const person = (name, add) => {
+//   return {
+//     personName: name,
+//     address: add,
+//     // bio: () => {
+//     bio() {
+//       return `The name is ${this.personName}`;
+//     },
+//   };
+// };
 
-const bikObj = person("John", "New York");
-console.log(bikObj.bio());
+// const bikObj = person("John", "New York");
+// console.log(bikObj.bio());
 
 // Class
+
+//Syntax
 // This keywords
 // Constructor
+class Person {
+  constructor(n, a) {
+    this.name = n;
+    this.add = a;
+  }
+}
+
+const bikObj = new Person("Bikal", "Sydney");
+console.log(bikObj);
+
+const jphnObj = new Person("Johhn", "London");
+console.log(jphnObj);
+
 // Encapsulation
 // Abstraction
 // Inheritance
