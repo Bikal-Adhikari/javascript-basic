fb17d24a0810e505bd0f320f576b51c4d280bec3
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-11 14:24:46 +1100

    Javascript DOM

---
+++ dom.js
@@ -0,0 +1,61 @@
+// document.getElementById(id)
+// document.getElementsByTagName(name)
+// document.getElementsByClassName(name)
+// document.querySelector
+
+// const elm = document.getElementById("first");
+// const elm = document.getElementsByClassName("info")[0];
+// console.log(elm.innerText);
+
+// const elm = document.querySelector("#first");
+// const elm = document.querySelectorAll(".info");
+// const elm = document.querySelector(".first > p");
+
+// const value = elm.innerText;
+// // console.log(value);
+// console.log(elm);
+
+// elm.innerText = "Bikal Adhikari";
+// // elm.style.fontSize = "2rem";
+// // elm.style.color = "red";
+// // elm.style.textDecoration = "underline";
+
+// // elm.setAttribute("href", "www.bikal.com");
+// elm.setAttribute("style", "color:red; font-size: 3rem");
+// elm.style.display = "none";
+
+// const elm = document.querySelector(".display");
+
+// const sayhey = () => {
+//   elm.innerText = "Hellow";
+//   elm.style.color = "red";
+//   elm.style.fontSize = "2rem";
+// };
+
+// // elm.addEventListener("click", sayhey);
+// elm.addEventListener("click", () => {
+//   elm.innerText = "Hellow";
+//   elm.style.color = "red";
+//   elm.style.fontSize = "2rem";
+// });
+
+// const anchor = document.querySelector("a");
+
+// anchor.innerText = "Facebook";
+// anchor.setAttribute("style", "text-decoration:none; font-size:2rem");
+// anchor.setAttribute("href", "https://www.facebook.com");
+// anchor.setAttribute("target", "_blank");
+
+// const elm = document.getElementsByClassName("message")[0];
+// elm.classList.add("success");
+
+// const newElm = document.createElement("p");
+// newElm.textContent = "New awesome";
+// elm.innerHTML += "<h1>Hello World!</h1>";
+// elm.appendChild(newElm);
+// elm.addEventListener("click", () => {
+//   elm.classList.toggle("success");
+// });
+// console.log(elm.classList);
+
+// document.write("<h2>Bikal Adhikari soft. engineer</h2>");
