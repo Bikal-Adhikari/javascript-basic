7a3b403f73634371e07a9d754a59f13cf1906c94
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-03-11 11:56:41 +1100

    FP high order function

--- script-advance.js
+++ script-advance.js
@@ -72,55 +72,129 @@
 // Inheritance
 // Polymorphism.
 
-class Living {
-  constructor({ name, age, dob, add }) {
-    this.name = name;
-    this.age = age;
-    this.dob = dob;
-    this.add = add;
-  }
-}
-class Human extends Living {
-  constructor({ partner, ...rest }) {
-    super(rest);
-    this.partner = partner;
-  }
+// class Living {
+//   constructor({ name, age, dob, add }) {
+//     this.name = name;
+//     this.age = age;
+//     this.dob = dob;
+//     this.add = add;
+//   }
+// }
+// class Human extends Living {
+//   constructor({ partner, ...rest }) {
+//     super(rest);
+//     this.partner = partner;
+//   }
 
-  bio() {
-    return `Hey, this is ${this.name} from ${this.add} and I am ${this.age} living with my partner ${this.partner}`;
-  }
-}
+//   bio() {
+//     return `Hey, this is ${this.name} from ${this.add} and I am ${this.age} living with my partner ${this.partner}`;
+//   }
+// }
 
-const samObj = {
-  name: "Sam",
-  age: 22,
-  dob: "2002",
-  add: "Sydney",
-  partner: "Emily",
+// const samObj = {
+//   name: "Sam",
+//   age: 22,
+//   dob: "2002",
+//   add: "Sydney",
+//   partner: "Emily",
+// };
+
+// const samInstance = new Human(samObj);
+
+// console.log(samInstance);
+
+// class Animal extends Living {
+//   constructor({ owner, ...rest }) {
+//     super(rest);
+//     this.owner = owner;
+//   }
+
+//   bio() {
+//     return `Hey, this is ${this.name} from ${this.add} and I am ${this.age} years old, living with its owner ${this.owner}`;
+//   }
+// }
+
+// const catObj = {
+//   name: "Polo",
+//   age: 2,
+//   dob: "2022",
+//   add: "Sydney",
+//   owner: "Merry",
+// };
+
+// const poloCat = new Animal(catObj);
+// console.log(poloCat.bio());
+
+// Functional Programming (FP)
+
+// const samObj = {
+//   name: "Sam",
+//   age: 22,
+//   dob: "2002",
+//   add: "Sydney",
+//   partner: "Emily",
+// };
+
+// const living = (name) => {
+//   return `Living with my partner ${name}`;
+// };
+// const bio = ({ name, add, age, partner }) => {
+//   return `Hey, this is ${name} from ${add} and I am ${age} years old, ${living(
+//     partner
+//   )}`;
+// };
+
+// const samBio = bio(samObj);
+// console.log(samBio);
+
+// Pure Function
+// let counter = 0;
+// const add = (x, y) => {
+//   counter++;
+//   return x + y;
+// };
+
+// add(2, 3);
+// add(2, 3);
+// const result = add(2, 3);
+// console.log(`The sum of 2 + 3 = ${result}`, counter);
+
+// Side effects
+
+// // High order function
+// [234, 456, 789].map((item, i) => {
+//   console.log(item, i);
+// });
+
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const subs = (a, b) => {
+//   return a - b;
+// };
+
+// const calc = (fn, a, b) => {
+//   return fn(a, b);
+// };
+
+// // const result = calc(add, 5, 6);
+// const result = calc(subs, 5, 6);
+// console.log(result);
+
+//add the values and return the multiply function
+const addAndMultiply = (num1, num2) => {
+  const sum = num1 + num2;
+  const multiply = (multiplier) => {
+    return sum * multiplier;
+  };
+
+  return multiply;
 };
 
-const samInstance = new Human(samObj);
-
-console.log(samInstance);
-
-class Animal extends Living {
-  constructor({ owner, ...rest }) {
-    super(rest);
-    this.owner = owner;
-  }
-
-  bio() {
-    return `Hey, this is ${this.name} from ${this.add} and I am ${this.age} years old, living with its owner ${this.owner}`;
-  }
-}
-
-const catObj = {
-  name: "Polo",
-  age: 2,
-  dob: "2022",
-  add: "Sydney",
-  owner: "Merry",
-};
-
-const poloCat = new Animal(catObj);
-console.log(poloCat.bio());
+const value = addAndMultiply(3, 5)(3);
+console.log(value);
+// console.log(value(1));
+// console.log(value(2));
+// console.log(value(3));
+// Recursion
